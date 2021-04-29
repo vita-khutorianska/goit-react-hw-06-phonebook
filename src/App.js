57779fea03a805d7ad9bdac9de44318d90792739
Vita Khutorianska <steblvita@gmail.com>
2021-04-29 12:32:14 +0300

    fix app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import shortid from 'shortid';
 import ContactForm from './Components/ContactForm';
 import ContactList from './Components/ContactList';
 import Filter from './Components/Filter';
-
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+import { connect } from 'react-redux';
+import store from './Redux/store';
+import contactsAction from './Redux/Phonebook/phonebook-action';
+function App({ items, filter, onAddContact }) {
+  // state = {
+  //   contacts: [
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   filter: '',
+  // };
   // componentDidMount() {
   //   const contacts = localStorage.getItem('contacts');
   //   const contactsParced = JSON.parse(contacts);
@@ -49,34 +51,44 @@ class App extends Component {
   //   });
   // };
 
-  filterChange = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
+  // filterChange = e => {
+  //   this.setState({ filter: e.currentTarget.value });
+  // };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
+  // deleteContact = id => {
+  //   this.setState(prevState => ({
+  //     contacts: prevState.contacts.filter(contact => contact.id !== id),
+  //   }));
+  // };
 
-  render() {
-    const { name, filter } = this.state;
-    const ContactFormFilter = this.state.filter.toLowerCase();
-    const filterNamePhoneBook = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(ContactFormFilter),
-    );
+  // render() {
+  // const { name, filter } = this.state;
+  // const ContactFormFilter = this.state.filter.toLowerCase();
+  // const filterNamePhoneBook = this.state.contacts.filter(contact =>
+  //   contact.name.toLowerCase().includes(ContactFormFilter),
+  // );
 
-    return (
+  // return (
+  //   <div>
+  //     <div>
+  //       <h1>Phonebook</h1>
+  //       <ContactForm />
+  //     </div>
+  //     <Filter />
+  //     <ContactList />
+  //   </div>
+  // );
+
+  return (
+    <div>
       <div>
-        <div>
-          <h1>Phonebook</h1>
-          <ContactForm />
-        </div>
-        <Filter value={filter} onChange={this.filterChange} />
-        <ContactList contacts={filterNamePhoneBook} />
+        <h1>Phonebook</h1>
+        <ContactForm />
       </div>
-    );
-  }
+      <Filter />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
