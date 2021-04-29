9692ff2f2710105920e212e2333226e3c8434138
Vita Khutorianska <steblvita@gmail.com>
2021-04-29 17:58:16 +0300

    add actions

--- src/Redux/Phonebook/phonebook-action.js
+++ src/Redux/Phonebook/phonebook-action.js
@@ -1,21 +1,27 @@
 import shortid from 'shortid';
 import types from './phonebook-types';
+import { createAction } from '@reduxjs/toolkit';
 
-const addContact = ({ name, number }) => ({
-  type: types.ADD,
-  payload: {
-    name,
-    number: number,
-    id: shortid.generate(),
-  },
-});
-const deleteContact = contactId => ({
-  type: types.DELETE,
-  payload: contactId,
-});
-const filterChange = value => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+// const addContact = ({ name, number }) => ({
+//   type: types.ADD,
+//   payload: {
+//     name,
+//     number: number,
+//     id: shortid.generate(),
+//   },
+// });
+// const deleteContact = contactId => ({
+//   type: types.DELETE,
+//   payload: contactId,
+// });
+// const filterChange = value => ({
+//   type: types.CHANGE_FILTER,
+//   payload: value,
+// });
 
+const addContact = createAction('contacts/addContact', ({ name, number }) => ({
+  payload: { id: shortid.generate(), name, number },
+}));
+export const filterChange = createAction('contacts/filter');
+export const deleteContact = createAction('contacts/delete');
 export default { addContact, deleteContact, filterChange };
